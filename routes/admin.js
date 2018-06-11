acdcf41ce349c504cacb65db4b3fa19719d9c22e
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-11 11:52:10 +0200

    remove, create, destroy admin

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,21 @@ const Router = Express.Router();
 
 Router.get('/',(req,res,next)=>{
    res.status(200).json(req.session.username || "Sesion no creada");
-
 })
 
 Router.get('/create',(req,res,next)=>{
     req.session.username="Luisito";
     res.redirect('/admin');
- 
+ })
+
+ Router.get('/remove',(req,res,next)=>{
+    req.session.username=null;
+    res.redirect('/admin');
+ })
+
+ Router.get('/destroy',(req,res,next)=>{
+    req.session.destroy();
+    res.redirect('/admin');
  })
 
 module.exports = Router;
