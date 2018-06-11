9cdff843f4a5b658e71ceb75177b997fdc063c0b
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-11 11:06:54 +0200

    username admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,14 @@ const Express = require('express');
 const Router = Express.Router();
 
 Router.get('/',(req,res,next)=>{
-   res.status(200).json("Se accede a admin");
+   res.status(200).json(req.session.username || "Sesion no creada");
 
 })
+
+Router.get('/create',(req,res,next)=>{
+    req.session.username="Luisito";
+    res.redirect('/admin');
+ 
+ })
+
 module.exports = Router;
